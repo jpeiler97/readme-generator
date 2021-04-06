76c1532002e04a8378f19b46fa4f0b1a3df934a7
JP Eiler <jp.eiler97@gmail.com>
2021-04-06 11:46:31 -0500

    commented fs.writeFile for testing purposes, added more readme items

--- index.js
+++ index.js
@@ -13,22 +13,37 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
 console.log('PROFESSIONAL README GENERATOR');
 console.log('*****************************');
 
 inquirer.prompt([
     {
-    type: 'input',
-    name: 'title',
-    message: 'What is your README\'s title?'
+        type: 'input',
+        name: 'title',
+        message: 'What is your README\'s title?'
     },
     {
-    type: 'input',
-    name: 'description',
-    message: 'Please type a description for your project.'
-    }])
+        type: 'input',
+        name: 'description',
+        message: 'Please type a description for your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please input the installation instructions for your project.'
+    },
+    {
+        type: 'Usage',
+        name: 'usage',
+        message: 'Please input the usage information for your project.'
+    }
+    ])
 .then((data) => {
-    const newText = `#${data.title} \n\n##Description\n${data.description}`
-    fs.writeFile('log.txt', newText, (err) => 
-    err ? console.log(err) : console.log('Success!'));
+    const tableOfContents = '[Installation](#Installation)\n[Usage](#Usage)\n[License](#License)\n[Contributing](#Contributing)\n[Tests](#Tests)\n[Questions](#Questions)';
+    const newText = `#${data.title}\n\n##Description\n${data.description}\n\n##Table of Contents\n${tableOfContents}\n`;
+    
+    console.log(newText);
+    // fs.writeFile('log.txt', newText, (err) => 
+    // err ? console.log(err) : console.log('Success!'));
 })
