4defd56bb35edbee8bc52c5dd46b6bc7eca8673d
JP Eiler <jp.eiler97@gmail.com>
2021-04-06 12:14:29 -0500

    added more input options

--- index.js
+++ index.js
@@ -34,14 +34,41 @@ inquirer.prompt([
         message: 'Please input the installation instructions for your project.'
     },
     {
-        type: 'Usage',
+        type: 'input',
         name: 'usage',
         message: 'Please input the usage information for your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please choose a license for your project.',
+        choices: ['MIT','ISC','GNU GPLv3','Apache 2.0']
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please input contribution guidelines for the project.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please input test instructions for the project.'
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Please input your gitHub username for the questions section.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please input your email address for the questions section.'
     }
     ])
 .then((data) => {
     const tableOfContents = '[Installation](#Installation)\n[Usage](#Usage)\n[License](#License)\n[Contributing](#Contributing)\n[Tests](#Tests)\n[Questions](#Questions)';
-    const newText = `#${data.title}\n\n##Description\n${data.description}\n\n##Table of Contents\n${tableOfContents}\n`;
+    const newText = `#${data.title}\n\n##Description\n${data.description}\n\n##Table of Contents\n${tableOfContents}\n\n##Installation\n${data.installation}\n\n##Usage\n${data.usage}\n\n
+    ##Contributing\n${data.contributing}\n\n##Tests\n${data.tests}\n\n##Questions\nFor further questions, you may reach me at:\n- GitHub: ${data.githubUsername}\n- EMail: ${data.email}`;
     
     console.log(newText);
     // fs.writeFile('log.txt', newText, (err) => 
