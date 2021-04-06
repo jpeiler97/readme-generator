4e2a5f184c0d8edcacd5e333a538d3dc2d58a615
JP Eiler <jp.eiler97@gmail.com>
2021-04-06 09:17:11 -0500

    worked on prompts for title and description

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
+/*
+1. Title
+2. Description
+3. Table of Contents
+4. Installation
+5. Usage
+6. License
+7. Contributing
+8. Tests
+9. Questions
+*/
+
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+console.log('PROFESSIONAL README GENERATOR');
+console.log('*****************************');
+
+inquirer.prompt([
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is your README\'s title?'
+    },
+    {
+    type: 'input',
+    name: 'description',
+    message: 'Please type a description for your project.'
+    }])
+.then((data) => {
+    const newText = `#${data.title} \n\n##Description\n${data.description}`
+    fs.writeFile('log.txt', newText, (err) => 
+    err ? console.log(err) : console.log('Success!'));
+})
