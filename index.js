01de33cfa58cd2ef9115229e893629f9dad872dc
JP Eiler <jp.eiler97@gmail.com>
2021-04-07 13:00:17 -0500

    added licenses

--- index.js
+++ index.js
@@ -73,10 +73,31 @@ inquirer.prompt([
     [Contributing](#Contributing)
     [Tests](#Tests)
     [Questions](#Questions)`;
+    
+    const licenses = [
+        {type: 'MIT',
+        badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'},
+        {type: 'ISC',
+        badge: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'},
+        {type: 'GNU GPLv3',
+        badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'},
+        {type: 'Apache 2.0',
+        badge: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'}];
+
+    let licenseChoice;
+
+    for(i = 0; i < licenses.length; i++){
+        if(data.license === licenses[i].type){
+            licenseChoice = licenses[i];
+        }
+    };
+
     const newText = 
     `
     #${data.title}
     
+    ${licenseChoice.badge}
+
     ##Description
     ${data.description}
 
@@ -89,6 +110,9 @@ inquirer.prompt([
     ##Usage
     ${data.usage}
 
+    ##License
+    This application is covered under the ${licenseChoice.type} license.
+
     ##Contributing
     ${data.contributing}
     
@@ -100,7 +124,7 @@ inquirer.prompt([
     - GitHub: ${data.githubUsername}
     - EMail: ${data.email}`;
     
-    // console.log(newText);
-    fs.writeFile('log.txt', newText, (err) => 
-    err ? console.log(err) : console.log('Success!'));
+    console.log(newText);
+    // fs.writeFile('log.txt', newText, (err) => 
+    // err ? console.log(err) : console.log('Success!'));
 })
